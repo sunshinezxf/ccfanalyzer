4ba70d9e02035e02c442d4b0ff55181c91b45638
nyaaar <nyaaarlathotep@163.com>
2021-01-20 17:14:31 +0800

    feat crawl xpath

--- papercrawl/papercrawl/spiders/ISSTA.py
+++ papercrawl/papercrawl/spiders/ISSTA.py
@@ -5,7 +5,11 @@ import scrapy
 class IsstaSpider(scrapy.Spider):
     name = 'ISSTA'
     allowed_domains = ['https://dblp.uni-trier.de/']
-    start_urls = ['http://https://dblp.uni-trier.de//']
+    start_urls = ['https://dblp.uni-trier.de/db/conf/issta/index.html']
 
     def parse(self, response):
-        pass
+        view_href = response.xpath('//*[@class="entry editor toc"]/nav/ul/li[1]/div[1]/a/@href').getall()
+        record_href = response.xpath('//*[@class="entry editor toc"]/nav/ul/li[2]/div[1]/a/@href').getall()
+        # self.logger.info('-------------------------------------------------------------------------------------------')
+        # self.logger.info(record_href)
+        # self.logger.info('-------------------------------------------------------------------------------------------')
