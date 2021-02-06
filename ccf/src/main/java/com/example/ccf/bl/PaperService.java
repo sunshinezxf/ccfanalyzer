76a791822f378a67050435ff07108d79cacb187a
nyaaar <nyaaarlathotep@163.com>
2021-02-06 18:33:57 +0800

    feat infoPaper
    还稍稍有点问题。。。后面看需求再改吧

--- ccf/src/main/java/com/example/ccf/bl/PaperService.java
+++ ccf/src/main/java/com/example/ccf/bl/PaperService.java
@@ -1,9 +1,12 @@
 package com.example.ccf.bl;
 
+import com.example.ccf.po.InfoPaper;
 import com.example.ccf.po.Paper;
 
 public interface PaperService {
 
+    InfoPaper getInfoPaperById(int paper_id);
 
     Paper getPaperById(int paper_id);
+
 }
--- ccf/src/main/java/com/example/ccf/data/paper/PaperMapper.java
+++ ccf/src/main/java/com/example/ccf/data/paper/PaperMapper.java
@@ -1,5 +1,6 @@
 package com.example.ccf.data.paper;
 
+import com.example.ccf.po.InfoPaper;
 import com.example.ccf.po.Paper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Repository;
 @Mapper
 @Repository
 public interface PaperMapper {
+
     Paper getPaperById(@Param("paper_id")int paper_id);
+
+    InfoPaper getInfoPaper(@Param("paper_id")int paper_id);
 
 }
