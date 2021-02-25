592326b416b235f8905ff68486f18fb4d7aae3ae
zhadrerr <837535316@qq.com>
2021-02-25 23:49:04 +0800

    增加注释

--- ccf/src/main/java/com/example/ccf/controller/search/SearchController.java
+++ ccf/src/main/java/com/example/ccf/controller/search/SearchController.java
@@ -5,6 +5,7 @@ import com.example.ccf.po.CommonSearch;
 import com.example.ccf.vo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
@@ -12,8 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class SearchController {
     @Autowired
     private SearchService searchService;
+    @CrossOrigin(origins="*",maxAge=3600)
     @RequestMapping(value ="/common",method = RequestMethod.GET)
     public ResponseVO commonSearch(CommonSearch commonSearch){return searchService.commonSearch(commonSearch);}
+    @CrossOrigin(origins="*",maxAge=3600)
     @RequestMapping(value ="/advanced",method = RequestMethod.GET)
     public ResponseVO advancedSearch(AdvancedSearch advancedSearch){return  searchService.advancedSearch(advancedSearch);}
 }
